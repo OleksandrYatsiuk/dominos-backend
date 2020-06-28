f241bf836b4050751b5ff17309bb04a70a21ec54
yatsiuk-oa <yatsiuk.oa@gbksoft.com>
2020-06-28 17:02:43 +0300

    remove min validation

--- src/rest/Promotions/promotions.validator.ts
+++ src/rest/Promotions/promotions.validator.ts
@@ -1,8 +1,8 @@
 import * as Joi from "@hapi/joi";
 
 export const promotion = Joi.object({
-    title: Joi.string().required().label('Title').max(100).min(10),
-    content: Joi.string().required().label('Content').max(1000).min(20),
+    title: Joi.string().required().label('Title').max(100),
+    content: Joi.string().required().label('Content').max(2000),
     image: Joi.string().required().label('Image')
 })
 
