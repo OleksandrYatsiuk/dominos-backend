65650c9d5297155563ca3fb01d2f7a926597bc33
yatsiuk-oa <yatsiuk.oa@gbksoft.com>
2020-06-29 14:08:02 +0300

    change validation

--- src/rest/Promotions/promotions.validator.ts
+++ src/rest/Promotions/promotions.validator.ts
@@ -5,7 +5,7 @@ export const promotion = Joi.object({
     title: Joi.string().required().label('Title').max(100),
     content: Joi.string().required().label('Content').max(2000),
     image: Joi.string().optional().label('Image'),
-    startedAt: Joi.number().required().label('Started Date'),
+    startedAt: Joi.date().required().label('Started Date'),
     status: Joi.number().label('Status').valid(PromotionStatuses.New, PromotionStatuses.Active, PromotionStatuses.Deactivate, PromotionStatuses.Finished)
 })
 
